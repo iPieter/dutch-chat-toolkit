daf9fc550c7c9f05e8e665db3618392ae20f2cae
Pieter Delobelle <pieter.delobelle@kuleuven.be>
2024-01-04 15:02:36 +0100

    Removed langchain memory store
    Due to langchain persisting conversations across users, we
    split up the chat history storage and the inference done
    by langchain.

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
-
 import argparse
+from operator import itemgetter
 
 import gradio as gr
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline, BitsAndBytesConfig
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    pipeline,
+    BitsAndBytesConfig,
+)
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.document_transformers import Html2TextTransformer
 from langchain.document_loaders import AsyncChromiumLoader
-from langchain.memory import ConversationBufferMemory
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
-from langchain.schema.runnable import RunnablePassthrough
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
 from langchain.schema import StrOutputParser
 from langchain.llms import HuggingFacePipeline
 from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferMemory
 
-CSS ="""
+CSS = """
 .contain { display: flex; flex-direction: column; }
 .gradio-container { height: 100vh !important; }
 #component-0 { height: 100%; }
 #chatbot { flex-grow: 1; overflow: auto;}
 """
 
-def main(args):
 
+def main(args):
     tokenizer = AutoTokenizer.from_pretrained(args.model_name, trust_remote_code=True)
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.padding_side = "right"
@@ -38,7 +41,6 @@ def main(args):
         args.model_name,
         quantization_config=bnb_config,
     )
-
 
     if args.load_from_scratch:
         # Loading sources
@@ -55,27 +57,52 @@ def main(args):
         html2text = Html2TextTransformer()
         docs_transformed = html2text.transform_documents(docs)
 
-        text_splitter = CharacterTextSplitter(chunk_size=args.chunk_size, 
-                                            chunk_overlap=0)
+        text_splitter = CharacterTextSplitter(
+            chunk_size=args.chunk_size, chunk_overlap=0
+        )
         chunked_documents = text_splitter.split_documents(docs_transformed)
 
+        db = FAISS.from_documents(
+            chunked_documents,
+            HuggingFaceEmbeddings(
+                model_name="NetherlandsForensicInstitute/robbert-2022-dutch-sentence-transformers"
+            ),
+        )
 
-        db = FAISS.from_documents(chunked_documents, 
-                                HuggingFaceEmbeddings(model_name='NetherlandsForensicInstitute/robbert-2022-dutch-sentence-transformers'))
-        
         print(f"Saving vectors to '{args.vectors_db}'.")
         db.save_local(folder_path=args.vectors_db)
 
     else:
-        db = FAISS.load_local(args.vectors_db,  HuggingFaceEmbeddings(model_name='NetherlandsForensicInstitute/robbert-2022-dutch-sentence-transformers'))
+        db = FAISS.load_local(
+            args.vectors_db,
+            HuggingFaceEmbeddings(
+                model_name="NetherlandsForensicInstitute/robbert-2022-dutch-sentence-transformers"
+            ),
+        )
 
     retriever = db.as_retriever()
 
+    pipe = pipeline(
+        "text-generation", model=model, tokenizer=tokenizer, max_new_tokens=1000
+    )
+    llm = HuggingFacePipeline(pipeline=pipe)
 
+    def format_docs(docs):
+        return "\n\n- ".join([d.page_content for d in docs])
+
+    def format_history(chat_history):
+        if chat_history and len(chat_history) > 0:
+            return (
+                "[/INST]"
+                + "\n".join(f"{human}\n[INST]{ai}[/INST]" for human, ai in chat_history)
+                + "\n[INST]\n"
+            )
+        else:
+            return ""
 
     # Create prompt template
     prompt_template = """
-    ### [INST] Je bent een expert in {topic}. Antwoord enkel in het Nederlands. Gebruik de volgende context voor vragen te beantwoorden:
+    [INST] Je bent een expert in {topic}. Antwoord enkel in het Nederlands. Gebruik de volgende context voor vragen te beantwoorden:
 
     {context}
     
@@ -86,60 +113,46 @@ def main(args):
     {question} 
     [/INST]"""
 
-    # Create prompt from prompt template 
     prompt = PromptTemplate(
-        input_variables=["context", "question"],
+        input_variables=["context", "question", "chat_history", "topic"],
         template=prompt_template,
     )
 
-    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=1000)
-    llm = HuggingFacePipeline(pipeline=pipe)
+    llm_chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
 
-    def format_docs(docs):
-        return "\n\n- ".join([d.page_content for d in docs])
-
-    def format_history(chat_history):
-        print(chat_history)
-        if len(chat_history) > 0:
-            return "[/INST]" + "\n".join(f"{human}\n[INST]{ai}[/INST]" for human, ai in chat_history) + "\n[INST]\n"
-
-
-    prompt = PromptTemplate(
-    input_variables=["context", "question", "chat_history"],
-        template=prompt_template,
-        )
-    memory = ConversationBufferMemory(memory_key="chat_history", input_key="question", human_prefix="\n[INST]\n", ai_prefix="[/INST]\n")
-
-
-    llm_chain = LLMChain(llm=llm, prompt=prompt, memory=memory, verbose=True)
-
-    rag_chain = ( 
-    {"context": retriever | format_docs, "question":  RunnablePassthrough()}
-        | llm_chain
-    )
+    rag_chain = {
+        "context": itemgetter("message") | retriever | format_docs,
+        "question": itemgetter("message"),
+        "chat_history": itemgetter("chat_history") | RunnableLambda(format_history),
+        "topic": itemgetter("topic"),
+    } | llm_chain
 
     with gr.Blocks(css=CSS) as demo:
 
         def respond(message, chat_history):
-            yield rag_chain.invoke(message)['text']
+            yield rag_chain.invoke(
+                {"message": message, "topic": args.topic, "chat_history": chat_history}
+            )["text"]
 
         chatbot = gr.ChatInterface(respond, title=args.title)
 
-        demo.launch(share=False)
+        demo.launch(share=args.share)
+
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(
-                    description='A Dutch retrieval-augmented chat toolkit',
-                    epilog='Developed by Pieter Delobelle (KU Leuven)')
-    
-    parser.add_argument('--load-from-scratch', action='store_true')
-    parser.add_argument('--model-name', default='mistralai/Mistral-7B-Instruct-v0.1')
-    parser.add_argument('--vectors-db', default='vectors/')
-    parser.add_argument('--chunk-size', default=1024)
-    parser.add_argument('--title', default="OudenaardeGPT")
-    parser.add_argument('--topic', default="de Oost-Vlaamse stad Oudenaarde")
-    
+        description="A Dutch retrieval-augmented chat toolkit",
+        epilog="Developed by Pieter Delobelle (KU Leuven)",
+    )
+
+    parser.add_argument("--load-from-scratch", action="store_true")
+    parser.add_argument("--share", action="store_true")
+    parser.add_argument("--model-name", default="mistralai/Mistral-7B-Instruct-v0.1")
+    parser.add_argument("--vectors-db", default="vectors/")
+    parser.add_argument("--chunk-size", default=1024)
+    parser.add_argument("--title", default="OudenaardeGPT")
+    parser.add_argument("--topic", default="de Oost-Vlaamse stad Oudenaarde")
+
     args = parser.parse_args()
-    
+
     main(args)
